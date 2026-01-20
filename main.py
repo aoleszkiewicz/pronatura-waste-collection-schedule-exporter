b6054982bb0018712be7ab8f8290b59107d499e2
Aleksander Oleszkiewicz <aleksander.oleszkiewicz0@gmail.com>
2026-01-20 22:51:48 +0100

    chore: remove unnecessary data

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 from schedule_client import ScheduleApiClient
 import pandas as pd
 
+import os
+from dotenv import load_dotenv
+from schedule_client import ScheduleApiClient
+import pandas as pd
+
 def main():
     load_dotenv()
 
     API_URL = os.getenv("API_URL")
     user_street_id = os.getenv("user_street_id")
     user_address_id = os.getenv("user_address_id")
-
-    user_street_id = "aa600d9a-6783-4936-9811-3e4870fffb4c"
-    user_address_id = "00cc1a01-ad5b-483e-8458-6680b0a34050"
 
     schedule_client = ScheduleApiClient(API_URL)
 
